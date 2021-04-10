bf801bccb8edfecc2c6ba81360816507d7a68fc3
DipWorks <63048134+DipWorks@users.noreply.github.com>
2021-04-10 13:23:42 +1000

    addin navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Home from "./components/Home";
+import React from "react";
+import Navbar from "./components/Navbar";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
-  return <Home />;
+  return (
+    <React.Fragment>
+      <Navbar />
+      <Home />
+    </React.Fragment>
+  );
 }
 
 export default App;
